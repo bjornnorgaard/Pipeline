8d26cd23d7d918fdccec9bf77d2e239927f8770f
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-08-20 13:40:27 +0200

    Added multiply service with unittests

---
+++ Api.Calculator/Calculator.Multiply.Test/MultiplyServiceUnittest.cs
@@ -0,0 +1,51 @@
+using Calculator.Add.Interface;
+using Calculator.Multiply.Service;
+using NSubstitute;
+using Xunit;
+
+namespace Calculator.Multiply.Test
+{
+    public class MultiplyServiceUnittest
+    {
+        private MultiplyService _uut;
+
+        public MultiplyServiceUnittest()
+        {
+            var addService = Substitute.For<IAddService>();
+
+            const int max = 1;
+            const int min = -1;
+            for (var i = min; i <= max; i++)
+            {
+                for (var j = min; j <= max; j++)
+                {
+                    addService.Add(i, j).Returns(i + j);
+                }
+            }
+
+            _uut = new MultiplyService(addService);
+        }
+
+        [Theory]
+        [InlineData(0, -1)]
+        [InlineData(0, 0)]
+        [InlineData(0, 1)]
+        [InlineData(1, -1)]
+        [InlineData(1, 0)]
+        [InlineData(1, 1)]
+        [InlineData(-1, -1)]
+        [InlineData(-1, 0)]
+        [InlineData(-1, 1)]
+        public void Multiply(int a, int b)
+        {
+            // Arrange
+            var expected = a * b;
+
+            // Act
+            var result = _uut.Multiply(a, b);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
